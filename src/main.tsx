0de61265b3d3646541536b7eeadcf75e826cf737
Fer280801 <fer.estradajr28@gmail.com>
2025-10-22 23:20:23 -0600

    Refactor: remove mocks and finalize login with motivational phrases

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-async function enableMocking() {
- if (process.env.NODE_ENV === "development") {
-  const { worker } = await import("./mocks/browser");
-  await worker.start({
-    onUnhandledRequest: "bypass",
-    serviceWorker: {
-      url: "/mockServiceWorker.js",
-    },
-  });
-}
-}
-
-enableMocking().then(() => {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-);
-});
+)
