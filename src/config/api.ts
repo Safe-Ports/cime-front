cc6f6c56090f7fe86767493f34c44a9aaa384a4f
Fer280801 <fer.estradajr28@gmail.com>
2025-11-07 14:50:47 -0600

    Revert "problema de conexion a la API"
    This reverts commit 0b5fdcf34f9133c7280e05a8041c7aba8df9e965.

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,27 +1,26 @@
 const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}`;
 
 export const API_ROUTES = {
-  AUTH: {
-    LOGIN: `${API_BASE_URL}/login/`,
-  },
-  PATIENTS: {
-    CREATE: `${API_BASE_URL}/patients/register`,
-    READ: `${API_BASE_URL}/patients`,
-    UPDATE: (patient_id: string) => `${API_BASE_URL}/patients/register/${patient_id}`,
-    DELETE: (patient_id: string) => `${API_BASE_URL}/patients/${patient_id}`,
-    HEALTH_RECORD: {
-      CREATE: `${API_BASE_URL}/patients/health_records`,
-      READ: `${API_BASE_URL}/patients/health_records`,
-      READ_BY_PATIENT_ID: `${API_BASE_URL}/patients/health_records`,
-      UPDATE: (patient_id: string) => `${API_BASE_URL}/patients/health_records/${patient_id}`,
-      DELETE: (patient_id: string) => `${API_BASE_URL}/patients/health_records/${patient_id}`,
+    AUTH: {
+        LOGIN: `${API_BASE_URL}/login/`,
     },
-  },
-
-  // CONSULTS: {
-  //   CREATE: `${API_BASE_URL}/consults/prescriptions`,
-  //   READ: `${API_BASE_URL}/consults/prescriptions`,
-  //   UPDATE: (prescription_id: string) => `${API_BASE_URL}/consults/prescriptions/${prescription_id}`,
-  //   DELETE: (prescription_id: string) => `${API_BASE_URL}/consults/prescriptions/${prescription_id}`,
-  // },
+    PATIENTS: {
+        CREATE: `${API_BASE_URL}/patients/register`,
+        READ: `${API_BASE_URL}/patients`,
+        UPDATE: (patient_id: string) => `${API_BASE_URL}/patients/register/${patient_id}`,
+        DELETE: (patient_id: string) => `${API_BASE_URL}/patients/${patient_id}`,
+        HEALTH_RECORD: {
+            CREATE: `${API_BASE_URL}/patients/health_records`,
+            READ: `${API_BASE_URL}/patients/health_records`,
+            READ_BY_PATIENT_ID: `${API_BASE_URL}/patients/health_records`,
+            UPDATE: (patient_id: string) => `${API_BASE_URL}/patients/health_records/${patient_id}`,
+            DELETE: (patient_id: string) => `${API_BASE_URL}/patients/health_records/${patient_id}`,
+        },
+    CONSULTS: {
+        CREATE: `${API_BASE_URL}/consults/prescriptions`,
+        READ: `${API_BASE_URL}/consults/prescriptions`,
+        UPDATE: (prescription_id: string) => `${API_BASE_URL}/consults/prescriptions/${prescription_id}`,
+        DELETE: (prescription_id: string) => `${API_BASE_URL}/consults/prescriptions/${prescription_id}`,
+    }
+    }
 };
