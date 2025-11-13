8e77323998da33a548f5b87045ccf7013ca85711
Fer280801 <fer.estradajr28@gmail.com>
2025-11-13 10:43:33 -0600

    .

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,7 +2,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const API_ROUTES = {
     AUTH: {
-        LOGIN: `${API_BASE_URL}/login/`,
+        LOGIN: `${API_BASE_URL}/login`,
     },
 
     PATIENTS: {
