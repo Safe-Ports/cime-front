f63ef74e9ca1ffb914b2c94761529d33d5fda5bc
Fer280801 <fer.estradajr28@gmail.com>
2025-11-13 10:46:52 -0600

    ..

--- src/config/api.ts
+++ src/config/api.ts
@@ -6,31 +6,31 @@ export const API_ROUTES = {
     },
 
     PATIENTS: {
-        CREATE: `${API_BASE_URL}/patients/register/`,
-        READ: `${API_BASE_URL}/patients/`,
+        CREATE: `${API_BASE_URL}/patients/register`,
+        READ: `${API_BASE_URL}/patients`,
         UPDATE: (patient_id: string) =>
-            `${API_BASE_URL}/patients/register/${patient_id}/`,
+            `${API_BASE_URL}/patients/register/${patient_id}`,
         DELETE: (patient_id: string) =>
-            `${API_BASE_URL}/patients/${patient_id}/`,
+            `${API_BASE_URL}/patients/${patient_id}`,
 
         HEALTH_RECORD: {
             CREATE: `${API_BASE_URL}/patients/health_records/`,
             READ: `${API_BASE_URL}/patients/health_records/`,
             READ_BY_PATIENT_ID: (patient_id: string) =>
-                `${API_BASE_URL}/patients/health_records/${patient_id}/`,
+                `${API_BASE_URL}/patients/health_records/${patient_id}`,
             UPDATE: (record_id: string) =>
-                `${API_BASE_URL}/patients/health_records/${record_id}/`,
+                `${API_BASE_URL}/patients/health_records/${record_id}`,
             DELETE: (record_id: string) =>
-                `${API_BASE_URL}/patients/health_records/${record_id}/`,
+                `${API_BASE_URL}/patients/health_records/${record_id}`,
         }
     },
 
     CONSULTS: {
-        CREATE: `${API_BASE_URL}/consults/prescriptions/`,
-        READ: `${API_BASE_URL}/consults/prescriptions/`,
+        CREATE: `${API_BASE_URL}/consults/prescriptions`,
+        READ: `${API_BASE_URL}/consults/prescriptions`,
         UPDATE: (prescription_id: string) =>
-            `${API_BASE_URL}/consults/prescriptions/${prescription_id}/`,
+            `${API_BASE_URL}/consults/prescriptions/${prescription_id}`,
         DELETE: (prescription_id: string) =>
-            `${API_BASE_URL}/consults/prescriptions/${prescription_id}/`,
+            `${API_BASE_URL}/consults/prescriptions/${prescription_id}`,
     }
 };
